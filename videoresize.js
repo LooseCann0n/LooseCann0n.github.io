f2dfc918a832c348cde261fdfb7829edd45cdec2
Thomas Billett <TB246112@falmouth.ac.uk>
2023-06-21 20:15:18 +0100

    Fixed embed video scaling

--- videoresize.js
+++ videoresize.js
@@ -2,22 +2,20 @@
   function onYouTubeIframeAPIReady() {
     var player;
 
- function resizePlayer() {
-  var container = document.querySelector('smallbox l');
-  var width = container.clientWidth;
-  var height = width * 0.6
-
-  player.setSize(width, height);
-}  
 player = new YT.Player('player', {
     height: '100%',
     width: '100%',
-    videoId: 'qgrneknSSBU',
+    videoID: 'qgrneknSSBU',
     events: {
-    'onReady': resizePlayer,
-    'onStateChange': resizePlayer
+      'onReady': onPlayerReady,
     }
+    
+    // videoId: 'qgrneknSSBU',
 });
-  
-  window.addEventListener('resize', resizePlayer);
+
+loadVideoById({'videoId': 'qgrneknSSBU',
+               'startSeconds': 5,
+               'endSeconds': 60,
+               'suggestedQuality': 'large'});
 }
+
